1841d9315637dfe57bcffd8a7a78a1ecae46e00e
Rogerio Vieira <46284511+rogeriovc1000@users.noreply.github.com>
2024-12-28 19:01:19 +0000

    Atualizações automáticas

--- app/cadastro_funcionarios.py
+++ app/cadastro_funcionarios.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e71bc4bad7c7cd7579882d0ec46d73c9bd59fbec0d6058544c2846e268d988a
-size 11257
+oid sha256:86c54ec0a8509f23d5ee419f85ec0686b301509034c07e811e18d5c7359f7538
+size 12009
